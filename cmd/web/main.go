ecd2a478ae26f9947935a5cd74b5c659e8e267b4
wangyao <wangyao.dream@hotmail.com>
2025-03-18 16:34:13 +0000

    change host

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Print("Starting server on :1080")
+	err := http.ListenAndServe("0.0.0.0:1080", mux)
 	log.Fatal(err)
 }
