c6cd85e35597fa974c0c6f9e7c3fa50857c2880a
wangyao <wangyao.dream@hotmail.com>
2025-03-19 15:51:00 +0800

    add file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,27 +9,33 @@ import (
 	"github.com/wangyaodream/snippetbox/internal/envutil"
 )
 
-type ServerConfig struct{
-    Host string
-    Port int
+type ServerConfig struct {
+	Host string
+	Port int
 }
 
 func main() {
-    // Get configuration
-    if err := godotenv.Load(".env"); err != nil {
-        log.Print("INFO: 未找到环境文件！将使用默认配置")
-    }
-    cfg := &ServerConfig{
-        Host: envutil.GetEnv("HOST", ""),
-        Port: envutil.GetInt("PORT", 4000),
-    }
+	// Get configuration
+	if err := godotenv.Load(".env"); err != nil {
+		log.Print("INFO: 未找到环境文件！将使用默认配置")
+	}
+	cfg := &ServerConfig{
+		Host: envutil.GetEnv("HOST", ""),
+		Port: envutil.GetInt("PORT", 4000),
+	}
+
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Printf("Starting server on :%d", cfg.Port)
-    hostStr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	hostStr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
 	err := http.ListenAndServe(hostStr, mux)
 	log.Fatal(err)
 }
