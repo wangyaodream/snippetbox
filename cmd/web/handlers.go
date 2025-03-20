d0b02124b5b412fd8f57c58949dc0951768e7ccf
wangyao <wangyao.dream@hotmail.com>
2025-03-20 17:25:05 +0800

    centralized error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -21,16 +21,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// 加载模板
 	ts, err := template.ParseFiles(tmplFiles...)
 	if err != nil {
-        app.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, err)
 		return
 	}
 
 	// 渲染模板
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, err)
 	}
 }
 
@@ -38,7 +36,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+        app.notFound(w)
 		return
 	}
 
@@ -49,7 +47,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
