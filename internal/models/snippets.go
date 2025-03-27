9a5fd6caaace2f687ac5d3b56d83068bca91b4c9
wangyao <wangyao.dream@hotmail.com>
2025-03-27 16:09:49 +0800

    add sql execution

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -6,6 +6,7 @@ import (
 )
 
 
+// Snippet represents a snippet of code
 type Snippet struct {
     ID int
     Title string
@@ -14,12 +15,33 @@ type Snippet struct {
     Expires time.Time
 }
 
+// SnippetModel defines a model type which wraps a sql.DB connection pool.
 type SnippetModel struct {
     DB *sql.DB
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-    return 0, nil
+    // 创建一个sql语句,INTERVAL ? DAY 为了让expires天数后过期
+    stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
+    `
+
+    // 使用Exec()方法执行sql语句
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, err
+    }
+
+    // 调用LastInsertId()方法获取最后插入的ID
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, err
+    }
+
+    // 返回ID
+    return int(id), nil
+
+
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
