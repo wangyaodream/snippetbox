20b1b0944add7bd24f0cf64826343e29a5a70480
wangyao <wangyao.dream@hotmail.com>
2025-03-28 11:25:37 +0800

    add model method get

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
+
+	"github.com/wangyaodream/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -13,53 +16,63 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    tmplFiles := []string{
-        "./ui/html/base.tmpl.html",
-        "./ui/html/partials/nav.tmpl.html",
-        "./ui/html/pages/home.tmpl.html",
-    }
+	tmplFiles := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
 	// 加载模板
 	ts, err := template.ParseFiles(tmplFiles...)
 	if err != nil {
-        app.serverError(w, err)
+		app.serverError(w, err)
 		return
 	}
 
 	// 渲染模板
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-        app.serverError(w, err)
+		app.serverError(w, err)
 	}
 }
-
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-        app.notFound(w)
+		app.notFound(w)
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
+	// 使用 SnippetModel 获取数据
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			// 这里捕获的是自定义的ErrNoRecord异常。
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
+	fmt.Fprintf(w, "%+v", snippet)
+}
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
-    title := "0 snail"
-    content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-    expires := 7
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
 
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, err)
-        return 
-    }
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,52 +2,69 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
-
 // Snippet represents a snippet of code
 type Snippet struct {
-    ID int
-    Title string
-    Content string
-    Created time.Time
-    Expires time.Time
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
 }
 
 // SnippetModel defines a model type which wraps a sql.DB connection pool.
 type SnippetModel struct {
-    DB *sql.DB
+	DB *sql.DB
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-    // 创建一个sql语句,INTERVAL ? DAY 为了让expires天数后过期
-    stmt := `INSERT INTO snippets (title, content, created, expires)
+	// 创建一个sql语句,INTERVAL ? DAY 为了让expires天数后过期
+	stmt := `INSERT INTO snippets (title, content, created, expires)
     VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
     `
 
-    // 使用Exec()方法执行sql语句
-    result, err := m.DB.Exec(stmt, title, content, expires)
-    if err != nil {
-        return 0, err
-    }
+	// 使用Exec()方法执行sql语句
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
 
-    // 调用LastInsertId()方法获取最后插入的ID
-    id, err := result.LastInsertId()
-    if err != nil {
-        return 0, err
-    }
+	// 调用LastInsertId()方法获取最后插入的ID
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
 
-    // 返回ID
-    return int(id), nil
-
+	// 返回ID
+	return int(id), nil
 
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-    return nil, nil
+	// 创建sql语句
+	stmt := `SELECT id, title, content, created, expires
+    FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+	// 使用QueryRow()方法执行sql语句
+	row := m.DB.QueryRow(stmt, id)
+	// 创建一个Snippet对象
+	s := &Snippet{}
+	// 使用Scan()方法将查询结果赋值给Snippet对象
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	// 返回Snippet对象
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-    return nil, nil
+	return nil, nil
 }
